93c74226326009d37bf469b6151589f9091322ca
HeadsetRuler <alph4numb3r@gmail.com>
2024-02-19 12:18:22 +0100

    GateKeeper v2

--- hello.go
+++ hello.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "time"
+
+func groet() string {
+    const phrase = "! Welkom bij Fonteyn Vakantieparken"
+    hour := time.Now().Hour()
+    switch {
+    case hour < 7:
+        return "Sorry, de parkeerplaats is ’s nachts gesloten"
+    case hour < 12:
+        return fmt.Sprintf("Goedemorgen%v", phrase)
+    case hour < 18: 
+        return fmt.Sprintf("Goedemiddag%v", phrase)
+    case hour < 23:
+        return fmt.Sprintf("Goedenavond%v", phrase)
+    default:
+        return "Sorry, de parkeerplaats is ’s nachts gesloten"
+    }
+}
 
 func main() {
-    fmt.Println("oh god please not again")
+    fmt.Println(groet())
 }
