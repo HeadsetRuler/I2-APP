5ffa304ac3abe8433c4e34bcef33d9dc042760d5
HeadsetRuler <alph4numb3r@gmail.com>
2024-02-26 10:46:49 +0100

    Added wrapper and dns tool

--- hello.go
+++ hello.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+    subcommands := map[string]*flag.FlagSet{
+        "help": flag.NewFlagSet("help", flag.ExitOnError),
+        "kenteken": flag.NewFlagSet("kentekenChecker", flag.ExitOnError),
+        "dns": flag.NewFlagSet("dns", flag.ExitOnError),
+    }
+    
+
+    if len(os.Args) < 2 {
+        fmt.Println("No command given, listing help")
+        help("")
+        return
+    }
+
+    switch os.Args[1] {
+    case "help":
+        subcommands["help"].Parse(os.Args[2:])
+        help(subcommands["help"].Arg(0))
+    case "kenteken":
+        kentekenChecker()
+    case "dns": 
+        dnsReverse := subcommands["dns"].Bool("r", false, "Reverse lookup")
+        subcommands["dns"].Parse(os.Args[2:])
+        dns(*dnsReverse, subcommands["dns"].Arg(0))
+    }
+}
 
 func groet() string {
     const phrase = "! Welkom bij Fonteyn Vakantieparken"
@@ -20,12 +52,36 @@ func groet() string {
     }
 }
 
-func main() {
+func help(Command string) {
+    switch Command {
+    case "kenteken":
+        fmt.Println("Usage: hello kenteken")
+        fmt.Println("Checks if a license plate is allowed to enter the parking lot")
+    case "help":
+        fmt.Println("Usage: hello help [command]")
+        fmt.Println("Shows help for the given command")
+    case "dns":
+        fmt.Println("Usage: hello dns [-r] address")
+        fmt.Println("Does a DNS lookup for the given address")
+        fmt.Println("  -r: Reverse lookup")
+    case "":
+        fallthrough
+    default:
+        fmt.Println("Usage: hello [command]")
+        fmt.Println("Available commands:")
+        fmt.Println("  kenteken: license plate checker")
+        fmt.Println("  help: show this help message")
+        fmt.Println("  dns: DNS lookup")
+    }
+}
+
+func kentekenChecker() {
     var kentekens = map[string]bool{
         "12-AB-34": true,
         "56-CD-78": true,
         "90-EF-12": true}
     var plate string
+    fmt.Print("Kenteken: ")
     n, err := fmt.Scan(&plate)
     if n == 0 || err != nil {
         fmt.Println("Geen kenteken opgegeven")
@@ -36,7 +92,25 @@ func main() {
     } else {
         fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
     }
+}
 
-
-    fmt.Println()
+func dns(reverse bool, address string){
+    if address == "" {
+        fmt.Println("No host/address given")
+        return
+    }
+    var result []string
+    var err error
+    if reverse {
+        result, err = net.LookupAddr(address)
+    } else {
+        result, err = net.LookupHost(address)
+    }
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    for _, r := range result {
+        fmt.Println(r)
+    }
 }
